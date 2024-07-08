e5ed3867e1503739df8faacda8eddeacdad3b560
visonn336 <visonn336@gmail.com>
2024-07-08 20:49:21 +0400

    Visonity 3.6 - The End?

--- help.php
+++ help.php
@@ -6,8 +6,9 @@
         <h2></h2>
         <h2>Bu web platformanın məqsədi sizin öz bilik və təcrübələrinizi paylaşmağınızdır.</h2>
         <h4>Elm paylaşdıqca artar. Siz müxtəlif məqalələr yazaraq həm başqalarına, həm də özünüzə böyük fayda verə bilərsiniz.</h4>
-        <h4>Müxtəlif yazarların məqalələrinə səs verərək fikrinizi bildirə bilər, məqalələri dostlarınızla paylaşa bilər və ya sonra oxumaq üçün qeyd edə bilərsiniz.</h4>
+        <h4>Müxtəlif yazarların məqalələrinə səs verərək fikrinizi bildirə bilər, onların profillərini ziyarət edə bilər, İdarə panelində məqalə yaza bilər vəya sadəcə oxuya biləriniz</h4>
         <h4>Kəşf Et kanalı ilə istədiyiniz mövzudakı məqalələri axtara, Populyar kanalı ilə isə ən çox səs almış məqalələrə baxa bilərsiniz.</h4>
+        <h4>Profil səhifəsində, Profil Şəklinizin üzərinə basaraq yeni şəkil seçə bilər, vəya İstifadəçi adınızın üzərinə basaraq profil məlumatlarınızı dəyişə bilərsiniz.</h4>
     </section>
 
 
